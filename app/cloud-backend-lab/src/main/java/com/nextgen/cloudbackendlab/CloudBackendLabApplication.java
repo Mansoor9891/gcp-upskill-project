181e1254abdee54a2ff1bb9d88de90b626a6595f
Mansoor Ahmed <mansoor9891@gmail.com>
2026-03-30 23:56:36 +0200

    day 1 spring boot & docker setup and run

---
+++ app/cloud-backend-lab/src/main/java/com/nextgen/cloudbackendlab/CloudBackendLabApplication.java
@@ -0,0 +1,13 @@
+package com.nextgen.cloudbackendlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBackendLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudBackendLabApplication.class, args);
+    }
+
+}
---
+++ app/cloud-backend-lab/src/test/java/com/nextgen/cloudbackendlab/CloudBackendLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextgen.cloudbackendlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudBackendLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
